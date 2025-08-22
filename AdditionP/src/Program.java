3144d2397665b4ff7ac1cb59aa933dd02fb297d2
Arbaz <arbazshaibu@gmail.com>
2025-08-22 23:51:07 +0530

    Addition

---
+++ AdditionP/src/Program.java
@@ -0,0 +1,11 @@
+public class Program {
+    public static void main(String[] args) {
+        int a=15;//variable a declared here
+        int b= 25;
+        int c=a+b;
+        a=45;//variable a reinitialized here
+        System.out.println(c);
+        System.out.println(a+b);
+    }
+    
+}
